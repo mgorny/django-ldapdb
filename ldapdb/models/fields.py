c02b6559a3cb0c9e3d1564e473ba4e1b27fd6086
Theo Chatzimichos <tampakrap@gentoo.org>
2013-07-08 01:53:06 +0200

    Add ACLField

--- ldapdb/models/fields.py
+++ ldapdb/models/fields.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-# 
+#
 # django-ldapdb
 # Copyright (c) 2009-2011, Bolloré telecom
 # All rights reserved.
-# 
+#
 # See AUTHORS file for a full list of contributors.
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-# 
-#     1. Redistributions of source code must retain the above copyright notice, 
+#
+#     1. Redistributions of source code must retain the above copyright notice,
 #        this list of conditions and the following disclaimer.
-#     
-#     2. Redistributions in binary form must reproduce the above copyright 
+#
+#     2. Redistributions in binary form must reproduce the above copyright
 #        notice, this list of conditions and the following disclaimer in the
 #        documentation and/or other materials provided with the distribution.
-# 
+#
 #     3. Neither the name of Bolloré telecom nor the names of its contributors
 #        may be used to endorse or promote products derived from this software
 #        without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -104,7 +104,7 @@ class IntegerField(fields.IntegerField):
             return 0
         else:
             return int(value[0])
-        
+
     def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
         "Returns field's value prepared for database lookup."
         return [self.get_prep_lookup(lookup_type, value)]
@@ -142,3 +142,25 @@ class ListField(fields.Field):
             return []
         return value
 
+class ACLField(fields.Field):
+    __metaclass__ = SubfieldBase
+
+    def from_ldap(self, value, connection):
+        name = self.name.split('_')[1] + '.group'
+        print name
+        print value
+        if name in value:
+            return True
+
+    def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
+        "Returns field's value prepared for database lookup."
+        return [self.get_prep_lookup(lookup_type, value)]
+
+    def get_db_prep_save(self, value, connection):
+        return [x.encode(connection.charset) for x in value]
+
+    def get_prep_lookup(self, lookup_type, value):
+        "Perform preliminary non-db specific lookup checks and conversions"
+        if lookup_type == 'contains':
+            return escape_ldap_filter(value)
+        raise TypeError("ACLField has invalid lookup: %s" % lookup_type)
