a54f30693f232d5623d8ae8c2f5408e4f480f813
Michał Górny <mgorny@gentoo.org>
2013-09-10 18:55:37 +0200

    Change version to 9999 to force setuptools to install the fork.
    If version matches the upstream version, setuptools download it from
    pypi. We need to use a different version to force it to fallback to
    custom sources.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "django-ldapdb",
-    version = "0.1.0",
+    version = "9999",
     #license = ldapdb.__license__,
     url = "http://opensource.bolloretelecom.eu/projects/django-ldapdb/",
     author = "Jeremy Laine",
